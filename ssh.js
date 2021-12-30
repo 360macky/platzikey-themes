323370ed1050a0d7ae4bc199aa5e1dbdf129e652
Marcelo Arias <hola@marceloarias.com>
2021-12-29 22:37:01 -0500

    Remove mode JavaScript files

--- ssh.js
+++
@@ -1,6 +0,0 @@
-let head = document.getElementsByTagName('head')[0];
-let link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href =
-  'https://fonts.googleapis.com/css2?family=Fira+Code:wght@300&display=swap';
-head.appendChild(link);
